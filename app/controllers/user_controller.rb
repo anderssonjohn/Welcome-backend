36e74ed426ee01618b25d35360a1908ff3d8c26a
John Andersson <exodusx32@gmail.com>
2016-09-22 13:41:09 +0200

    Add route to get user by id

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,11 +1,17 @@
 class UserController < ApplicationController
-  skip_before_action  :verify_authenticity_token
+  #skip_before_action  :verify_authenticity_token
 
   def create
     user = User.create(first_name: params[:first_name],
       last_name: params[:last_name],
       proffesion: params[:proffesion])
     user.valid? ? user.save : puts("ERROR: INVALID USER")
+  end
+
+  def show
+      user = User.find(params[:id])
+
+      render :json => user
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/user/:first_name/:last_name/:proffesion' => 'user#create'
+
+  get '/user/:id/' => 'user#show'
 end
