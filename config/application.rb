58e3cb1ba674cdfd71f37cd2f630d6a7546d1054
John Andersson <exodusx32@gmail.com>
2016-09-22 13:40:46 +0200

    Change application to API

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,15 @@
 require_relative 'boot'
 
 require 'rails/all'
-encoding = "utf-8"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Backend
   class Application < Rails::Application
+    config.api_only = true
+
+    config.debug_exception_response_format = :default
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
